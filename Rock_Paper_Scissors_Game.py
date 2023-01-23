80f3007b26fa40cea54e675ac1176c2c017b7737
PaFkataPetrov <122817181+PaFkataPetrov@users.noreply.github.com>
2023-01-23 10:44:10 +0200

    Game_Test

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -5,36 +5,40 @@ rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    print("Invalid Input. Try again: ")
-    raise SystemExit
+player_move = ""
 
-computer_random_choose = random.randint(1, 3)
+while player_move != "end":
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-computer_move = ""
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        print("Invalid Input. Try again: ")
+        raise SystemExit
 
-if computer_random_choose == 1:
-    computer_move = rock
-elif computer_random_choose == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    computer_random_choose = random.randint(1, 3)
 
-print(f"The computer chose {computer_move}")
+    computer_move = ""
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print("\033[1;32;40m You win!\n")
-elif player_move == computer_move:
-    print("\033[1;37;40m Draw!\n")
-else:
-    print("\033[1;31;40m You lose!\n")
+    if computer_random_choose == 1:
+        computer_move = rock
+    elif computer_random_choose == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
+
+    print(f"The computer chose {computer_move}")
+
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print("\033[1;32;40m You win!\n")
+    elif player_move == computer_move:
+        print("\033[1;37;40m Draw!\n")
+    else:
+        print("\033[1;31;40m You lose!\n")
