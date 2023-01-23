23b4319adb00ea0ae646c349560bdae8abf217a0
PaFkataPetrov <122817181+PaFkataPetrov@users.noreply.github.com>
2023-01-23 13:10:10 +0200

    Added colorama for colors changing

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -1,14 +1,15 @@
 import random
-
+import colorama
+colorama.init()
 
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 
-
 player_move = ""
 
 while player_move != "end":
+    print(colorama.Style.RESET_ALL)
     player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
     if player_move == "r":
@@ -31,14 +32,13 @@ while player_move != "end":
         computer_move = paper
     else:
         computer_move = scissors
-
-    print(f"The computer chose {computer_move}")
+    print(colorama.Fore.YELLOW, f"The computer chose {computer_move}")
 
     if (player_move == rock and computer_move == scissors) or \
             (player_move == paper and computer_move == rock) or \
             (player_move == scissors and computer_move == paper):
-        print("\033[1;32;40m You win!\n")
+        print(colorama.Fore.GREEN, "You win!")
     elif player_move == computer_move:
-        print("\033[1;37;40m Draw!\n")
+        print(colorama.Fore.BLUE, "Draw!")
     else:
-        print("\033[1;31;40m You lose!\n")
+        print(colorama.Fore.RED, "You lose!")
