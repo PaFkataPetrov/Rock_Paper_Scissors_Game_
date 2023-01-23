2f234c658e703924c5ccfc6db7c929c2925c48b9
PaFkataPetrov <122817181+PaFkataPetrov@users.noreply.github.com>
2023-01-23 10:40:21 +0200

    Game_Test

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -1,5 +1,6 @@
 import random
 
+
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
@@ -32,8 +33,8 @@ print(f"The computer chose {computer_move}")
 if (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-    print("You win!")
+    print("\033[1;32;40m You win!\n")
 elif player_move == computer_move:
-    print("Draw!")
+    print("\033[1;37;40m Draw!\n")
 else:
-    print("You lose!")
+    print("\033[1;31;40m You lose!\n")
